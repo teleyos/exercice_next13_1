53584ccac3ef74ec149324fe1aaa015048845730
tristan <tristanduret@hotmail.Fr>
2023-08-23 13:15:06 +0200

    fixing IconButton props warning

--- src/app/_components/EditableControls.tsx
+++ src/app/_components/EditableControls.tsx
@@ -1,20 +1,35 @@
 'use client'
-import { ButtonGroup, Flex, IconButton, useEditableControls } from '@chakra-ui/react'
+import {
+  ButtonGroup,
+  Flex,
+  IconButton,
+  IconButtonProps,
+  useEditableControls
+} from '@chakra-ui/react'
 import { MdCheck, MdClose, MdEdit } from 'react-icons/md'
 
 const EditableControls = () => {
   const { isEditing, getSubmitButtonProps, getCancelButtonProps, getEditButtonProps } =
     useEditableControls()
 
-  // @ts-ignore
   return isEditing ? (
     <ButtonGroup justifyContent='center' size='sm'>
-      <IconButton icon={<MdCheck />} {...getSubmitButtonProps()} />
-      <IconButton icon={<MdClose />} {...getCancelButtonProps()} />
+      <IconButton
+        icon={<MdCheck />}
+        {...(getSubmitButtonProps() as Omit<IconButtonProps, 'icon'>)}
+      />
+      <IconButton
+        icon={<MdClose />}
+        {...(getCancelButtonProps() as Omit<IconButtonProps, 'icon'>)}
+      />
     </ButtonGroup>
   ) : (
     <Flex justifyContent='center'>
-      <IconButton size='sm' icon={<MdEdit />} {...getEditButtonProps()} />
+      <IconButton
+        size='sm'
+        icon={<MdEdit />}
+        {...(getEditButtonProps() as Omit<IconButtonProps, 'icon' | 'size'>)}
+      />
     </Flex>
   )
 }
